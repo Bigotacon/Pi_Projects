875e9696afa8037c053d1f5615b437750e00f44e
Bigotacon <jpicaro138@gmail.com>
2016-12-03 14:15:02 -0500

    Deleted write.csv to keep temp logs. Updated Senses.py to calibrate the temp logs as the cpu heats up.

--- Senses.py
+++ Senses.py
@@ -9,10 +9,13 @@ date_measured = strftime("%Y-%m-%d", localtime())
 time_measured = strftime("%I:%M:%S %p", localtime())
 
 sense = SenseHat()
+cpu_temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1000
 temp = sense.get_temperature()
+temp_calibrated = temp - ((cpu_temp - temp)/5.466)
+
 hum = sense.get_humidity()
 
-d = {'date':date_measured, 'time':time_measured, 'temperature':temp, 'humidity': hum}
+d = {'date':date_measured, 'time':time_measured, 'temperature':temp_calibrated, 'humidity': hum}
 df = pd.DataFrame(data=d, index=[datetime_measured])
 df = df[cols]
 df.index.name = 'datetime'
