6e09c598ca50cef7ee8002d9383935d5349ffedc
Bigotacon <jpicaro138@gmail.com>
2016-12-10 11:37:02 -0500

    This commit adds an AT&T M2X solution. senses_to_m2x.py - has been added to the project iotClass.py - procedures have been moved to senses_to_m2x.py

---
+++ senses_to_m2x.py
@@ -0,0 +1,21 @@
+from iotClass import IotDevice, attM2x
+from sense_hat import SenseHat
+
+
+def main():
+    """this function calls all objects and methods"""
+    sense = SenseHat()
+    temp = sense.get_temperature()
+    hum = sense.get_humidity()
+    pressure = sense.get_pressure()
+
+    FACTOR = 5.466
+    temp_calibrated = temp - ((cpu_temp - temp)/FACTOR)
+    streamDictionary = {'temperature': temp_calibrated}
+    m2xSenseHatRasperryPi3 = attM2x("Rarpberry Pi",
+                                    "SenseHat_Raspberry_Py3",
+                                    "04be6de5865d3d9d95a6dbd7182d3083",
+                                    "241bedffb719af49f3b296b783ca5d49")
+    m2xSenseHatRasperryPi3.put_json(streamDictionary)
+
+main()
