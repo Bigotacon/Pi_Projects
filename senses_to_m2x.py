15e2ce5d0e0202dc4fbc24f5813d61838d0c9c34
Bigotacon <jpicaro138@gmail.com>
2016-12-19 11:40:15 -0500

    senses_to_m2x.py - simplified how cpu temp and hyper local temp is converted in the code.

--- senses_to_m2x.py
+++ senses_to_m2x.py
@@ -9,12 +9,11 @@ def main():
     temp = sense.get_temperature()
     hum = sense.get_humidity()
     pressure = sense.get_pressure()
-
-    cpu_temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1000
     FACTOR = 5.466
-    temp_calibrated = temp - ((cpu_temp - temp)/FACTOR)
-    temp_calibrated  = (temp_calibrated * 1.8) + 32
 
+    cpu_temp = (int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1000) * 1.8 + 32)
+    temp  = (temp * 1.8) + 32
+    temp_calibrated = temp - ((cpu_temp - temp)/FACTOR)
 
     api_get = apiManager('RaspberryPi', 'wunderground_api', api_key_dic['wunderground'])
     api_get.set_wunderground_measurement('RI', 'Tiverton', 'conditions')
@@ -22,7 +21,8 @@ def main():
 
     streamDictionary = {'temperature': temp_calibrated, 'humidity': hum,
                         'pressure':pressure,'tiv_temperature':api_get.local_temp_f,
-                        'tiv_pressure': api_get.local_pressure, 'tiv_humidity': api_get.local_relative_humidity}
+                        'tiv_pressure': api_get.local_pressure, 'tiv_humidity': api_get.local_relative_humidity,
+                        'cpu_temp': cpu_temp }
     m2xSenseHatRasperryPi3 = attM2x('Rarpberry Pi',
                                     'SenseHat_Raspberry_Py3',
                                     '04be6de5865d3d9d95a6dbd7182d3083',
